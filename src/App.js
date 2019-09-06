39081874fef9e5419ce23d7a34581b4bbb73397a
PaulinaFranco <dadiante@gmail.com>
2019-09-06 10:14:40 -0500

    Se integra login y acceso a home

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import firebaseInit from './firebase';
+import Home from './home';
+import Login from './login.js';
 
-function App() {
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
+  }
+
+componentDidMount() {
+  this.authListener();
+}
+
+authListener() {
+  firebaseInit.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    if (user) {
+      this.setState({ user });
+      localStorage.setItem('user', user.uid);
+    } else {
+      this.setState({ user: null });
+      localStorage.removeItem( 'user' );
+    }
+  });
+}
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hola
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{this.state.user ? (<Home/>) : (<Login/>)}</div>
   );
+}
 }
 
 export default App;
